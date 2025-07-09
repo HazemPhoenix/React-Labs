e67a2c87a4b00f4ba888ecc296691166732729a9
hazemphoenix <hazem.mando408@gmail.com>
2025-07-09 13:28:24 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
 import "./App.css";
-import Hero from "./features/day1/Hero/components/Hero";
-import Bio from "./features/day1/Bio/components/Bio";
-import Skills from "./features/day1/Skills/components/Skills";
-import Projects from "./features/day1/Projects/components/Projects";
-import Contacts from "./features/day1/Contacts/components/Contacts";
-
 function App() {
-  return (
-    <>
-      <Hero />
-      <Bio />
-      <Skills />
-      <Projects />
-      <Contacts />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
